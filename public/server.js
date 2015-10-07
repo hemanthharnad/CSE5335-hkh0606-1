0b5bf87e0d25c073aa161864321665d0bb4a249b
Hemanth Kumar Harnad <hemanthharnad@gmail.com>
2015-10-07 02:19:17 -0500

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,4 +1,4 @@
-var http = require('http'),
+/*var http = require('http'),
     fs = require('fs'),
     url = require('url');
 
@@ -20,4 +20,29 @@ http.createServer(function(req, res) {
         res.end();
     });
 }).listen(port,"http://localhost");
-console.log("server initialized");
+console.log("server initialized");*/
+var http = require("http");
+var fs = require("fs");
+
+//We will send them a 404 response if page doesn't exist
+function send404Response(response){
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("Error 404 - Page not found");
+    response.end();
+}
+
+//Handle their request
+function onRequest(request, response) {
+
+    if( request.method == 'GET' && request.url == '/' ){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        //Open file as readable stream, pipe stream to response object
+        fs.createReadStream("./index.html").pipe(response);
+    }else{
+        send404Response(response);
+    }
+
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server is now running...");
