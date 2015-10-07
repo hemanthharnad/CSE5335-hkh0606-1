804dbc8dbcf1dcabd98369f441d2d65c9869fcc1
Hemanth Kumar Harnad <hemanthharnad@gmail.com>
2015-10-07 02:31:07 -0500

    Update server.js

--- public/server.js
+++ public/server.js
@@ -24,19 +24,19 @@ console.log("server initialized");*/
 var http = require("http");
 var fs = require("fs");
 
-//We will send them a 404 response if page doesn't exist
+
 function send404Response(response){
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.write("Error 404 - Page not found");
     response.end();
 }
 
-//Handle their request
+
 function onRequest(request, response) {
 
     if( request.method == 'GET' && request.url == '/' ){
         response.writeHead(200, {"Content-Type": "text/html"});
-        //Open file as readable stream, pipe stream to response object
+        
         fs.createReadStream("./index.html").pipe(response);
     }else{
         send404Response(response);
@@ -44,5 +44,5 @@ function onRequest(request, response) {
 
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("Server is now running...");
+http.createServer(onRequest).listen(3000);
+
